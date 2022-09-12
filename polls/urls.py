60d50803b5e3caacd0897b0c51fe95b197ab663e
Siripa-Maneein <siripa.m@ku.th>
2022-09-12 10:17:46 +0700

    Redirect to index page showing error message when navigating not allowed voting page.

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ app_name = 'polls'
 urlpatterns = [
     path('polls/', views.IndexView.as_view(), name='index'),
     path('', views.BaseIndexView.as_view(), name='redirect-index'),
-    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/', views.detail, name='detail'),
     path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse, path
 from django.views import generic
 from django.contrib import messages
+from django.http import Http404
 from django.views.generic import RedirectView
 
 
@@ -10,7 +11,7 @@ from .models import Question, Choice
 
 
 class BaseIndexView(generic.DetailView):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         """When the base url is called, it will be redirect to polls index."""
         return HttpResponseRedirect(reverse('polls:index'))
 
@@ -30,16 +31,18 @@ class IndexView(generic.ListView):
         ).order_by('-pub_date')[:5]
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Includes questions that can be voted only.
-        """
-        can_vote_id_list = [q.id for q in Question.objects.all() if q.can_vote()]
-        return Question.objects.filter(id__in=can_vote_id_list)
+def detail(request, pk):
+    try:
+        question = Question.objects.get(pk=pk)
+        if not question.can_vote():
+            messages.error(request, "Voting is not allowed for this question.")
+            return HttpResponseRedirect(reverse('polls:index'))
+        return render(request, 'polls/detail.html', {
+            'question': question
+        })
+    except Question.DoesNotExist:
+        messages.error(request, "The question you're looking for does not existed.")
+        return HttpResponseRedirect(reverse('polls:index'))
 
 
 class ResultsView(generic.DetailView):
