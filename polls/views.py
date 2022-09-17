88aa550cc39b0503500a038d41494f47af61b6f1
Siripa-Maneein <siripa.m@ku.th>
2022-09-17 22:12:11 +0700

    Add decorator and Mixin so that user must login to vote.

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Question, Choice
 
@@ -34,19 +35,23 @@ class IndexView(generic.ListView):
         ).order_by('-pub_date')[:5]
 
 
-def detail(request, pk):
-    """Return correct response to detail view request."""
-    try:
-        question = Question.objects.get(pk=pk)
-        if not question.can_vote():
-            messages.error(request, "‼️ Voting is not allowed for this question.")
+class DetailView(LoginRequiredMixin, generic.DetailView):
+    """A class for detail view of a poll."""
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            question = Question.objects.get(pk=self.kwargs['pk'])
+            if not question.can_vote():
+                messages.error(request, "‼️ Voting is not allowed for this question.")
+                return HttpResponseRedirect(reverse('polls:index'))
+            return render(request, 'polls/detail.html', {
+                'question': question
+            })
+        except Question.DoesNotExist:
+            messages.error(request, "‼️ The question you're looking for does not exist.")
             return HttpResponseRedirect(reverse('polls:index'))
-        return render(request, 'polls/detail.html', {
-            'question': question
-        })
-    except Question.DoesNotExist:
-        messages.error(request, "‼️ The question you're looking for does not exist.")
-        return HttpResponseRedirect(reverse('polls:index'))
 
 
 class ResultsView(generic.DetailView):
@@ -61,6 +66,7 @@ class ResultsView(generic.DetailView):
         return Question.objects.filter(id__in=published_id_list)
 
 
+@login_required
 def vote(request, question_id):
     """Return correct response to vote view request."""
     question = get_object_or_404(Question, pk=question_id)
